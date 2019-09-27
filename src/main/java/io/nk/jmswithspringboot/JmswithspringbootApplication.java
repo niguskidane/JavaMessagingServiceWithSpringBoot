467255f94f7ea50304367f260901dd3dc446ec5c
King <nigus.kidane@gmail.com>
2019-09-26 20:17:48 -0600

    Spring Boot Project with JMS dependecy created

---
+++ src/main/java/io/nk/jmswithspringboot/JmswithspringbootApplication.java
@@ -0,0 +1,13 @@
+package io.nk.jmswithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmswithspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmswithspringbootApplication.class, args);
+    }
+
+}
